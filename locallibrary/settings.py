ee2a664d9d326bd1a13868352c7cfe29d44cf778
Nguyen Thinh <ng.tr.thinh@gmail.com>
2021-07-25 14:02:18 +0700

    Add permission to host on heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://quiet-everglades-37040.herokuapp.com']
 
 
 # Application definition
